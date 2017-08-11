c3063bbff5bb8031c53be52943df333cd9d7e155
Asami Doi <doiasami1219@gmail.com>
2017-08-11 15:20:23 +0900

    update alert hello world

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log("hello" + tab.url);
+    console.log('Turning ' + tab.url + ' red!');
+    alert('hello world!');
+    chrome.tabs.executeScript({
+	code: 'document.body.style.backgroundColor="red"'
+    });
 });
